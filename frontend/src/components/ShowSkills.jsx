95db49a09edf177f84fabccc347ca004a33e2a89
speedyfish <brianlimjj5@gmail.com>
2022-10-03 17:54:38 +0800

    Frontend

---
+++ frontend/src/components/ShowSkills.jsx
@@ -0,0 +1,20 @@
+import { useGlobalContext } from '../context'
+
+const ShowSkills = () => {
+  const { selectedMeal, closeModal } = useGlobalContext()
+
+  return <aside className='modal-overlay'>
+    <div className='modal-container'>
+      {/* <img src={image} className="img modal-img" />
+      <div className='modal-content'>
+        <h4>{title}</h4>
+        <p>Cooking Instructions</p>
+        <p> {text}</p>
+        <a href={source} target="_blank">Original Source</a>
+        <button className="btn btn-hipster close-btn" onClick={closeModal}>close</button>
+      </div> */}
+    </div>
+  </aside>
+}
+
+export default ShowSkills
