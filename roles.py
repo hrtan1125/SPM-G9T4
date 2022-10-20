88c075dd0e20b8168d70d8b3fd59bc7e5ce1e56e
hrtan1125 <hrtan1125@gmail.com>
2022-10-20 19:32:41 +0800

    edit function viewRoleSkills

--- roles.py
+++ roles.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import and_
 from flask_cors import CORS
-from skills import Skills
+from skills import viewSkillsByRole 
+# import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:' + \
                                         '@localhost:3306/projectDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
@@ -32,32 +35,36 @@ class Roles(db.Model):
             result[column] = getattr(self, column)
         return result
 
-
 class Role_Skills(db.Model):
     __tablename__ = 'role_skills'
-
     rowid = db.Column(db.Integer, primary_key=True)
-    role_id = db.Column(db.Integer)
-    skill_code = db.Column(db.String(20))
+    role_id = db.Column(db.Integer, nullable=False)
+    skill_code = db.Column(db.String(20), nullable=True)
 
     def to_dict(self):
         """
-        'to_dict' converts the object into a dictionary,
-        in which the keys correspond to database columns
+            'to_dict' converts the object into a dictionary,
+            in which the keys correspond to database columns
         """
+
         columns = self.__mapper__.column_attrs.keys()
         result = {}
         for column in columns:
             result[column] = getattr(self, column)
         return result
 
+    def __init__(self, role_id, skill_code):
+        self.role_id = role_id
+        self.skill_code = skill_code
+
+
 # admin create role
 @app.route("/create", methods=['POST'])
 def createRole():
     data = request.get_json()
-    
+    # print(data)
     # check if data format is correct
-    if not "role_name" in data.keys():
+    if not "role_name" in data.keys() or not "skills" in data.keys():
         return jsonify(
             {
                 "message": "Incorrect data format"
@@ -74,13 +81,6 @@ def createRole():
                 }
             ), 400
 
-    # if not all(key in data.keys() for
-    #            key in ('role_name', 'deleted')):
-    #     return jsonify(
-    #         {
-    #             "message": "Incorrect data format."
-    #         }
-    #     ), 404
     role = Roles(**{"role_name":newRole,"deleted":"no"})
 
     try:
@@ -88,33 +88,50 @@ def createRole():
         db.session.flush()
 
         # add the role with skills
-        if not "skills" in data.keys():
-            return jsonify({
-                "message": "missing skills to be assigned."
-            })
+        # if not "skills" in data.keys():
+        #     return jsonify({
+        #         "message": "missing skills to be assigned."
+        #     })
             
         skillsList = data['skills']
         assignSkill(skillsList,role.role_id)
 
         # commit everything together, to the role table and the skill table
-        # this can be move to be done in the assignSkills function instead as well
-        db.session.commit()
+        # this can be moved to the assignSkills function instead
+        # db.session.commit()
 
-        return jsonify(role.to_dict()), 201
+        return jsonify(role.to_dict()), 200
     except Exception:
         return jsonify({
             "message": "Unable to commit to database."
-        }), 400
+        }), 500
 
 # admin read all roles
 @app.route("/view")
 def viewRoles():
-    data = Roles.query.all()
+    data = Roles.query.filter_by(deleted="no").all()
     return jsonify(
         {
             "data": [role.to_dict() for role in data]
         }
     ), 200
+
+    # admin read all roles
+@app.route("/viewselectedrole")
+def viewSelectedRole():
+    try:
+        # data = request.get_json()
+        # role_id = data["role_id"]
+
+        role_id = request.args["role_id"]
+        selectedRole = Roles.query.filter_by(role_id=role_id).first()
+        return jsonify(selectedRole.to_dict()), 201
+    except Exception:
+        return jsonify(
+            {
+                "message": "Unexpected Error."
+            }
+        ),500
 
 # admin update a role
 @app.route("/update", methods=['PUT'])
@@ -156,42 +173,99 @@ def updateRole():
             {
                 "message": "Unexpected Error."
             }
-        ), 400
+        ), 500
 
 # admin delete role (soft delete)
 @app.route("/delete", methods=['PUT'])
 def removeRole():
-    # multi-select delete
     try:
         data = request.get_json()
-        roles = data["roles"]
-        RolesToDelete = Roles.query.filter(Roles.role_id.in_(roles))
-
-        for role in RolesToDelete:
-            role.deleted = "yes"
+        role_id = data["role_id"]
+        roleToDelete = Roles.query.filter_by(role_id=role_id).first()
+        roleToDelete.deleted = "yes"
 
         db.session.commit()
         return jsonify({
-            "message": "Roles have been removed!"
-        })
+            "message": "Role has been removed!"
+        }), 200
 
     except Exception:
         return jsonify(
             {
                 "message": "Unexpected Error."
             }
-        )
+        ), 500
 
-# admin assign skills, still need changes
-# directly call from http
+
+#assign skills to role
 @app.route("/assignSkills", methods=['POST'])
-def assignSkill(skillslist, role_id):
-    for skill in skillslist:
-        roleData = Role_Skills(**{"role_id": role_id,"skill_code":skill})
-        db.session.add(roleData)
-    
-    # alternatively, do db.session.commit() here
-    return
+def assignSkill(skillslist=[], role_id=0):
+    data = request.get_json()
 
+    if data:
+        if all(key in data.keys() for
+                    key in ('role_id', 'skill_code')):
+            print(data['role_id'])            
+            role_id = data["role_id"]
+            skillslist = data["skill_code"]
+
+    for skill in skillslist:
+        if (Role_Skills.query.filter_by(skill_code=skill, role_id=role_id).first()):
+            return jsonify(
+                {
+                    "code": 400,
+                    "data": {
+                        "skill_code": skill,
+                        "role_id": role_id
+                    },
+                    "message": "Skill is already assigned to role."
+                }
+            ), 400
+
+        else:
+            roleData = Role_Skills(**{"role_id": role_id,"skill_code":skill})
+
+        try:
+            db.session.add(roleData)
+            db.session.commit()
+
+        except SQLAlchemyError as e:
+            print(e)
+            db.session.rollback()
+            return jsonify(
+                {
+                    "code": 500,
+                    "data": {
+                        "skill_code": skill,
+                        "role_id": role_id
+                    },
+                    "message": "An error occurred when assigning the skill to role."
+                }
+            ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Successful assignment of skill(s) to role"
+        }
+    ), 201
+
+@app.route("/viewRoleSkills", methods=['GET'])
+def viewRoleSkills():
+    search_skill = request.args.get('role_id')
+    try:
+        if search_skill:
+            RoleSkills = Role_Skills.query.filter_by(role_id=search_skill).all()
+            skills = viewSkillsByRole(RoleSkills)
+            return skills
+        else:
+            return jsonify({
+                "message": "Missing Input."
+            }), 400
+    except Exception:
+        return jsonify({
+            "message": "Unable to commit to database."
+        }), 500
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
