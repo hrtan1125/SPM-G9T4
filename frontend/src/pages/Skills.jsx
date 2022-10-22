c6c3b0cf7151b5a465fde8a8649aba006d9c0ca2
mb-zm <89783284+mb-zm@users.noreply.github.com>
2022-10-22 14:57:43 +0800

    Updated Skills.jsx

--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -5,6 +5,14 @@ import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import EditIcon from '@mui/icons-material/Edit';
 import { Button, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 const Skills = () => {
   const {setPath} = useGlobalContext()
@@ -21,53 +29,58 @@ const Skills = () => {
 
     return (
       <div style={{display: 'flex', marginTop: 80, justifyContent: "center"}} >
-         <div className="app-container">
-         <Link to={`/createskill`}> 
-          <Button onClick={resetSkill} variant="contained">Create New Skill</Button>
-         </Link>
-        <table>
-          <thead>
-            <tr>
-              <th>Skill Code</th>
-              <th>Skill Name</th>
-              <th>Deleted?</th>
-              <th>Delete</th>
-              <th>Edit</th>
-
-            </tr>
-          </thead>
-          <tbody>
-          {skills.map((skill) => (
-            <tr key={skill.skill_code}>
+        <div className="app-container" style={{display: 'flex',justifyContent:"center"}}>
+          <div style={{display: 'flex', justifyContent: "center"}}>
+            <Link to={`/createskill`} style={{textDecoration:"none"}}> 
+            <Button onClick={resetSkill} style={{backgroundColor:"#5289B5"}} variant="contained">Create New Skill</Button>
+            </Link>
+          </div>
+          <TableContainer component={Paper}>
+          {/* TableContainer */}
+          <Table sx={{ minWidth: 640, "& td": { border: 0 }}} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Skill Code</TableCell>
+                <TableCell>Skill Name</TableCell>
+                <TableCell>Deleted?</TableCell>
+                <TableCell>Delete</TableCell>
+                <TableCell>Edit</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {skills.map((skill) => (
+              <TableRow key={skill.skill_code} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               {skill.deleted === "no" && (
                 <>
-                <td>
+                <TableCell>
                   {skill.skill_code}
-                </td>
-                <td>
+                </TableCell>
+                <TableCell>
                   {skill.skill_name}
-                </td>
-                <td>
+                </TableCell>
+                <TableCell>
                   {skill.deleted === "yes" ? ("Yes"): ("No")}
-                </td>
-                <td>
+                </TableCell>
+                <TableCell>
                   <Grid item xs={8}>
                     <DeleteOutlinedIcon onClick={() => deleteSkill(skill.skill_code)}/>
                   </Grid>
-                </td>
-                <td>
+                </TableCell>
+                <TableCell>
                   <Grid item xs={8}>
                     <Link to={`/skill/${skill.skill_code}/${skill.skill_name}`}>
                       <EditIcon/>
                     </Link>
                   </Grid>
-                </td>
+                </TableCell>
                 </>
               )}
-            </tr>
+            </TableRow>
           ))}
-          </tbody>
-        </table>
+          </TableBody>
+        </Table>
+          </TableContainer>
+          
         </div>
       </div>
     );
