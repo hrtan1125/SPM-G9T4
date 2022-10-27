5bde87a836258bb01eae80ee1e5485937dc4c01a
hrtan1125 <hrtan1125@gmail.com>
2022-10-28 00:25:33 +0800

    Update CreateEditRole.jsx
    Edit input field

--- frontend/src/components/CreateEditRole.jsx
+++ frontend/src/components/CreateEditRole.jsx
@@ -1,7 +1,7 @@
 import { Button, TextField, Grid, Chip } from '@mui/material';
 import { useEffect } from 'react';
-import {React,} from 'react'
-import { useState } from 'react';
+import {React, createRef} from 'react'
+import { useState, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useGlobalContext } from '../context';
 
@@ -12,8 +12,9 @@ var toDeleteSkills = [];
 const CreateEditRole = () =>{
 
   const { relatedSkills, setRoleId, updateRole, skills, setPath } = useGlobalContext()
+  const roleRef = useRef(null);
+
   useEffect(()=>setPath("Roles"),[])
-  console.log(count++,"runs")
 
     let navigate = useNavigate();
     const {role_id, role_name} = useParams()
@@ -25,29 +26,23 @@ const CreateEditRole = () =>{
       }
     },[])
     
-    const [roleName, setRoleName] = useState(role_name)
-
-    function handleChange(event) {
-        setRoleName(event.target.value)
-    }
 
     const handleUpdateSubmit = (e) => {
       e.preventDefault();
       updateRole({
         "role_id": role_id,
-        "role_name": roleName
+        "role_name": roleRef.current.value
       });
       // navigate(`/Roles`);
     };
 
-    console.log(role_id)
-
 
 function refreshPage() {
+  navigate(`/${role_id}/${roleRef.current.value}`)
   window.location.reload(false);
-  navigate(`/${role_id}/${roleName}`)
 }
 
+//handle any updates on click
 function handleUpdate(e,key){
   if(Object.keys(relatedSkills).includes(key)){
     if (toDeleteSkills.includes(key)){
@@ -76,6 +71,7 @@ function handleUpdate(e,key){
   console.log("these will be deleted",toDeleteSkills)
 }
 
+//submit new role to database
 const createNewRole=async(roleName,skillsToAssign)=>{
   console.log("Creating new Role")
   fetch('http://127.0.0.1:5001/create',{
@@ -97,12 +93,12 @@ const handleSubmit = (e) => {
   e.preventDefault();
   console.log("Event is", e.target.textContent)
   if(e.target.textContent==="Create"){
-    if (!roleName){
+    if (!roleRef.current.value){
       alert("Role Name cannot be empty")
     }else if (toUpdateSkills.length === 0){
       alert("Please select at least one skill")
     }else{
-      createNewRole(roleName,toUpdateSkills)
+      createNewRole(roleRef.current.value,toUpdateSkills)
     }
     
     
@@ -121,8 +117,8 @@ const handleSubmit = (e) => {
       assignSkillsToRole(role_id, toUpdateSkills)
     }
 
-    if (roleName !== role_name) {
-      console.log("Role Name has been changed to", roleName)
+    if (roleRef.current.value !== role_name) {
+      console.log("Role Name has been changed to", roleRef.current.value)
       handleUpdateSubmit(e)
       refreshPage();
     }
@@ -174,7 +170,7 @@ const deleteSkillsFromRole = async(role_id, skill_code) => {
     <Grid container spacing={2} >
       <Grid item sx={{display:"flex", alignItems:"center"}}> 
       Role Name:   &nbsp;
-        <TextField size="small" id="outlined-basic"  variant="outlined" value={roleName} onChange={handleChange}/>  
+        <TextField size="small" id="outlined-basic"  variant="outlined" defaultValue={role_name} inputRef={roleRef}/>  
       </Grid>
       <Grid item sx={{display:"flex", alignItems:"center"}}>
       <Button style={{backgroundColor:"#5289B5"}} variant="contained" onClick={(e) => handleSubmit(e)}>{role_id?"Edit":"Create"}</Button>
@@ -184,7 +180,7 @@ const deleteSkillsFromRole = async(role_id, skill_code) => {
     Selects skills to add....  
     </Grid>
     <Grid container spacing={2} marginTop={1}>
-{(Object.keys(relatedSkills).length!==0 || typeof(role_id)==="undefined") && skills.length!==0  && skills.map((skill)=>(
+{skills.length!==0  && skills.map((skill)=>(
   <Chip key={skill.skill_code} label={skill.skill_name} sx={{margin:1}} onClick={(e)=>handleUpdate(e,skill.skill_code)} variant={Object.keys(relatedSkills).includes(skill.skill_code)?"filled":"outlined"}/>))}
   </Grid>
   </>
