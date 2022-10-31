eab423d8c015ba37ebb94de1a09b28fa554ed1cf
yyz2020 <89073448+yyz2020@users.noreply.github.com>
2022-11-01 04:56:44 +0800

    staff unit test

--- learning_journey_unit_test.py
+++ learning_journey_unit_test.py
@@ -25,6 +25,22 @@ class TestLearningJourneyProgress(unittest.TestCase):
         with self.assertRaises(Exception):
             courses_and_statuses = []
             progress = Learning_Journey.calculate_progress(courses_and_statuses)
-    
+class TestStaff(unittest.TestCase):
+    def setUp(self):
+        self.staff1 = Staff(Staff_ID=130001,Staff_FName="John",Staff_LName="Sim",
+        Dept="Chairman",Email="jack.sim@allinone.com.sg",Role=1)
+
+    def tearDown(self):
+        self.staff1 = None
+
+    def test_to_dict(self):
+        self.assertDictEqual(self.staff1.to_dict(),{
+            "Staff_ID": 130001,
+            "Staff_FName":"John",
+            "Staff_LName": "Sim",
+            "Dept": "Chairman",
+            "Email": "jack.sim@allinone.com.sg",
+            "Role": 1 
+        })
 if __name__ == "__main__":
     unittest.main()
