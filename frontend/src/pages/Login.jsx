144ee65677da524cd849ebb069e8e3a5669dd8dc
speedyfish <brianlimjj5@gmail.com>
2022-11-03 10:20:41 +0800

    Fixed Login Bug

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,15 +5,12 @@ import Box from '@mui/material/Box';
 
 import { Button, TextField } from '@mui/material';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const {setPath, userRole, setUserRole, setUser, setUserDept} = useGlobalContext()
+  const {setPath, setUserRole, setUser, setUserDept} = useGlobalContext()
   useEffect(()=>setPath("Login"),[])
   useEffect(()=>  setUserRole(""),[])
   useEffect(()=>  localStorage.setItem("userRole", 0),[])
-
-  let navigate = useNavigate();
 
   const [staffId, setStaffId] = React.useState('');
 
@@ -27,20 +24,17 @@ const Login = () => {
     console.log(staffId, "STAFFFIDD")
     axios.get(`${getUserRoleUrl}${staffId}`)
     .then(response => {console.log(response.data.role, "KEKEKEKE")
-      // if (!(response.data.role > 0 && response.data.role < 4)) return
       localStorage.setItem("userRole", response.data.role)
       localStorage.setItem("userDept", response.data.dept)
       localStorage.setItem("user", staffId)
       setUser(staffId)
       setUserRole(response.data.role)
       setUserDept(response.data.dept)
+      window.location.replace("http://localhost:3000/learningjourneys");
     })
     .catch(error => {
-      
-        console.error('There was an error!', error);
-        
+        console.error('There was an error!', error);   
     });
-    navigate(`/learningjourneys`);
   };
 
 
