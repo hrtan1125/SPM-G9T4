1fb36a7c72be8ef4e661e5153652bf2ffa69aa70
mb-zm <89783284+mb-zm@users.noreply.github.com>
2022-11-05 22:51:07 +0800

    updated skill integration tests again

--- skills_integration_tests.py
+++ skills_integration_tests.py
@@ -24,12 +24,6 @@ class TestApp(flask_testing.TestCase):
 class testSkills(TestApp):
     def test_view_skill(self):
 
-        # from skills import Skills, db
-
-        # request_body = {
-        #     'skill_name':'Cost Management level 3'
-        # }
-
         s1 = Skills(skill_code="BF002", skill_name="Cost Management level 3", deleted="no")
         s2 = Skills(skill_code="BOM001", skill_name="Strategy Development level 3", deleted="no")
         s3 = Skills(skill_code="COR111", skill_name="Software Project Management", deleted="yes")
@@ -59,26 +53,26 @@ class testSkills(TestApp):
         })
         
 class testViewSelectedSkill(TestApp):
-        def test_view_selected_skill(self):
+    def test_view_selected_skill(self):
 
-            s1 = Skills(skill_code="BF002", skill_name="Cost Management level 3", deleted="no")
-            # s2 = Skills(skill_code="BOM001", skill_name="Strategy Development level 3", deleted="no")
-            s3 = Skills(skill_code="COR111", skill_name="Software Project Management", deleted="yes")
+        s1 = Skills(skill_code="BF002", skill_name="Cost Management level 3", deleted="no")
+        s2 = Skills(skill_code="BOM001", skill_name="Strategy Development level 3", deleted="no")
+        s3 = Skills(skill_code="COR111", skill_name="Software Project Management", deleted="yes")
 
-            db.session.add(s1)
-            # db.session.add(s2)
-            db.session.add(s3)
-            db.session.commit()
+        db.session.add(s1)
+        db.session.add(s2)
+        db.session.add(s3)
+        db.session.commit()
 
-            response = self.client.get("/viewselectedskill?skill_code=BF002", 
-                                        content_type="application/json")
+        response = self.client.get("/viewselectedskill?skill_code=BF002", 
+                                    content_type="application/json")
 
-            self.assertEqual(response.status_code,201)
-            self.assertDictEqual(response.json, {
-                "skill_code": "BF002",
-                "skill_name": "Cost Management level 3",
-                "deleted": "no"
-            })
+        self.assertEqual(response.status_code,201)
+        self.assertDictEqual(response.json, {
+            "skill_code": "BF002",
+            "skill_name": "Cost Management level 3",
+            "deleted": "no"
+        })
 
 
 class testEditSkill(TestApp):
@@ -154,7 +148,7 @@ class testEditSkill(TestApp):
 
 #delete role
 class testDeleteSkill(TestApp):
-    def test_delete_role(self):
+    def test_delete_existing_skill(self):
         s1 = Skills(skill_code="BF002", skill_name="Cost Management level 3", deleted="no")
 
         db.session.add(s1)
@@ -172,7 +166,29 @@ class testDeleteSkill(TestApp):
         self.assertDictEqual(response.json,{
             "message": "Skill has been removed!"
         })
+    
+    def test_delete_non_existing_skill(self):
+        s1 = Skills(skill_code="BF002", skill_name="Cost Management level 3", deleted="no")
+        s2 = Skills(skill_code="BOM001", skill_name="Strategy Development level 3", deleted="no")
+        s3 = Skills(skill_code="COR111", skill_name="Software Project Management", deleted="yes")
 
+        db.session.add(s1)
+        db.session.add(s2)
+        db.session.add(s3)
+        db.session.commit()
+
+        request_body = {
+            'skill_code':'COR111'
+        }
+
+        response = self.client.put("/delete",
+                                    data=json.dumps(request_body),
+                                    content_type='application/json')
+
+        self.assertEqual(response.status_code,400)
+        self.assertDictEqual(response.json,{
+            "message": "Skill no longer available."
+        })
 
 if __name__ == '__main__':
     unittest.main()
