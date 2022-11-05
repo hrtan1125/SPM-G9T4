c29d2ac069d8661ebccee910afeece9570b7a6ae
hrtan1125 <hrtan1125@gmail.com>
2022-11-05 18:34:38 +0800

    update create/edit skill page

--- frontend/src/components/CreateEditSkill.jsx
+++ frontend/src/components/CreateEditSkill.jsx
@@ -4,53 +4,95 @@ import { Routes, Route, useParams, useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../context';
 
 const CreateEditSkill = () => {
+  const updateSkillUrl = 'http://127.0.0.1:5000/update'
+  const createSkillUrl = 'http://127.0.0.1:5000/create'
   let navigate = useNavigate();
-    const {skill_code, skill_name} = useParams()
-    console.log("SKILLNAME", skill_name)
+  const { skill_code, skill_name } = useParams()
 
-    const {updateSkill, createSkill} = useGlobalContext()
-
-    const [formData, setFormData] = React.useState(
-      {skill_name: skill_name, skill_code: skill_code}
+  const [status, setStatus] = useState(null)
+  const [formData, setFormData] = React.useState(
+    { skill_name: skill_name, skill_code: skill_code }
   )
 
-    function handleChange(event) {
-      setFormData(prevFormData => {
-          return {
-              ...prevFormData,
-              [event.target.name]: event.target.value
-          }
-      })
+  function handleChange(event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+      }
+    })
   }
   console.log(formData, "formmdataa")
+
+  useEffect(()=>{
+    if(status===400){
+      alert("Skill Name/Code has been used! Please try a new one.")
+    }else if(status===200||status===201){
+      navigate(`/Skills`);
+    }
+  },[status])
+
+  const createSkill = async (formData) => {
+    try {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      };
+      fetch(createSkillUrl, requestOptions)
+        .then(response => {
+          setStatus(response.status)
+          return response.json()
+        })
+
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
+  const updateSkill = async (formData) => {
+    try {
+      const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      };
+      fetch(updateSkillUrl, requestOptions)
+        .then(response => {
+          setStatus(response.status)
+          return response.json()
+        })
+
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
 
   const handleUpdateSubmit = (e) => {
     e.preventDefault();
     updateSkill(formData);
-    navigate(`/Skills`);
- };
+  };
 
- const handleCreateSubmit = (e) => {
-  e.preventDefault();
-  createSkill(formData);
-  navigate(`/Skills`);
-};
+  const handleCreateSubmit = (e) => {
+    e.preventDefault();
+    createSkill(formData);
+  };
 
 
 
 
   return (
-    <div style={{display: 'flex', marginTop: 80, justifyContent: "center"}} >
+    <div style={{ display: 'flex', marginTop: 80, justifyContent: "center" }} >
 
-            <div>
-                {skill_name === undefined && (<>Skill Code: <TextField id="outlined-basic" style={{width:"100px"}} size="small" variant="outlined" name="skill_code" value={formData.skill_code} onChange={handleChange} /></> )}
-            </div>
-            <div>
-              Skill Name: <TextField id="outlined-basic" style={{width:"400px"}} variant="outlined" name="skill_name" size="small" value={formData.skill_name} onChange={handleChange} />
-            </div>
-            <div>
-              {skill_name === undefined ? (<Button style={{backgroundColor:"#5289B5"}} onClick={(e) => handleCreateSubmit(e)} variant="contained">Create Skill</Button>):(<Button style={{backgroundColor:"#5289B5"}} onClick={(e) => handleUpdateSubmit(e)} variant="contained">Update Skill Name</Button>)}
-            </div>
+      <div>
+        {skill_name === undefined && (<>Skill Code: <TextField id="outlined-basic" style={{ width: "100px" }} size="small" variant="outlined" name="skill_code" value={formData.skill_code} onChange={handleChange} /></>)}
+      </div>
+      <div>
+        Skill Name: <TextField id="outlined-basic" style={{ width: "400px" }} variant="outlined" name="skill_name" size="small" value={formData.skill_name} onChange={handleChange} />
+      </div>
+      <div>
+        {skill_name === undefined ? (<Button style={{ backgroundColor: "#5289B5" }} onClick={(e) => handleCreateSubmit(e)} variant="contained">Create Skill</Button>) : (<Button style={{ backgroundColor: "#5289B5" }} onClick={(e) => handleUpdateSubmit(e)} variant="contained">Update Skill Name</Button>)}
+      </div>
     </div>
   )
 }
--- frontend/src/context.jsx
+++ frontend/src/context.jsx
@@ -26,233 +26,234 @@ const viewLJsUrl = 'http://127.0.0.1:5002/viewlearningjourneys?staff_id='
 const viewCoursesByLJ = 'http://127.0.0.1:5002/viewCoursesByLearningJourney?'
 
 const AppProvider = ({ children }) => {
-    const [path, setPath] = useState("Learning Journey")
+  const [path, setPath] = useState("Learning Journey")
 
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    const [userDetails, setUserDetails] = useState({})
+  const [userDetails, setUserDetails] = useState({})
 
-    useEffect(()=>setUserDetails(JSON.parse(localStorage.getItem("userDetails"))),[])
+  useEffect(() => setUserDetails(JSON.parse(localStorage.getItem("userDetails"))), [])
 
-    
-    
-    // Roles
-    const [roles, setRoles] = useState([])
-    const [role, setRole] = useState([])
-    const [roleId, setRoleId] = useState('')
 
-    // Skills
-    const [skills, setSkills] = useState([])
-    const [relatedSkills, setRelatedSkills] = useState([])
-    const [skill, setSkill] = useState([])
-    const [skillCode, setSkillCode] = useState('')
-    
-    // Courses
-    const [lid, setLid] = useState(0)
-    const [ltitle, setLTitle] = useState("")
-    const [courses, setCourses] = useState([])
-    const [allCourses, setAllCourses] = useState([])
-    const [addCourses, setAddCourses] = useState([])
 
-    // Learning Journey Pages
-    const [ljCourses, setljCourses] = useState({})
-    const [showModal, setShowModal] = useState(false)
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [skipped, setSkipped] = React.useState(new Set());
+  // Roles
+  const [roles, setRoles] = useState([])
+  const [role, setRole] = useState([])
+  const [roleId, setRoleId] = useState('')
 
-    const closeModal = () => {
-      setShowModal(false)
-    }
+  // Skills
+  const [skills, setSkills] = useState([])
+  const [relatedSkills, setRelatedSkills] = useState([])
+  const [skill, setSkill] = useState([])
+  const [skillCode, setSkillCode] = useState('')
 
-    const fetchRoles = async(url) => {
-        try {
-            const {data} = await axios(url)
-            setRoles(data.data)
-        } catch (error) {
-            console.log(error.response)
-        }
-    }
-    const fetchRole = async(url) => {
-      try {
-          const {data} = await axios(url)
-          setRole(data)
-      } catch (error) {
-          console.log(error.response)
-      }
+  // Courses
+  const [lid, setLid] = useState(0)
+  const [ltitle, setLTitle] = useState("")
+  const [courses, setCourses] = useState([])
+  const [allCourses, setAllCourses] = useState([])
+  const [addCourses, setAddCourses] = useState([])
+
+  // Learning Journey Pages
+  const [ljCourses, setljCourses] = useState({})
+  const [showModal, setShowModal] = useState(false)
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
+  const closeModal = () => {
+    setShowModal(false)
   }
 
-  const fetchRelatedSkills = async(url) => {
+  const fetchRoles = async (url) => {
     try {
-        const {data} = await axios(url)
-        setRelatedSkills(data.data)
-        console.log("RS:",data.data)
+      const { data } = await axios(url)
+      setRoles(data.data)
     } catch (error) {
-        console.log(error.response)
+      console.log(error.response)
     }
-}
+  }
+  const fetchRole = async (url) => {
+    try {
+      const { data } = await axios(url)
+      setRole(data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
+  const fetchRelatedSkills = async (url) => {
+    try {
+      const { data } = await axios(url)
+      setRelatedSkills(data.data)
+      console.log("RS:", data.data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
 
 
-const fetchRelatedCourses = async(url) => {
-  try {
-      const {data} = await axios(url)
+  const fetchRelatedCourses = async (url) => {
+    try {
+      const { data } = await axios(url)
       setCourses(data.data)
-  } catch (error) {
-      console.log(error.response)
-  }
-}
-  
-  const fetchSkills = async(url) => {
-    try {
-        const {data} = await axios(url)
-        setSkills(()=>{return data.data})
     } catch (error) {
-        console.log(error.response)
-    }
-}
-
-const fetchCourses = async(url) => {
-  try {
-      const {data} = await axios(url)
-      setAllCourses(data.data)
-  } catch (error) {
       console.log(error.response)
+    }
   }
-}
 
-const fetchSkill = async(url) => {
-  try {
-      const {data} = await axios(url)
+  const fetchSkills = async (url) => {
+    try {
+      const { data } = await axios(url)
+      setSkills(() => { return data.data })
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
+  const fetchCourses = async (url) => {
+    try {
+      const { data } = await axios(url)
+      setAllCourses(data.data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
+  const fetchSkill = async (url) => {
+    try {
+      const { data } = await axios(url)
       console.log("fetching skill", data)
       setSkill(data)
-      
-  } catch (error) {
+
+    } catch (error) {
       console.log(error.response)
+    }
   }
-}
 
-    useEffect(() => {
-        fetchRoles(rolesUrl)
-        console.log("test fetch role")
-    }, [])
-    useEffect(() => {
-      fetchSkills(skillsUrl)
+  useEffect(() => {
+    fetchRoles(rolesUrl)
+    console.log("test fetch role")
   }, [])
-    useEffect(() => {
-      fetchCourses(coursesUrl)
-    }, [])
-      useEffect(() => {
-        setRelatedSkills(()=>[])
-        if (roleId){
-          
-          fetchRole(`${viewSelectedRoleUrl}${roleId}`)
-        }
-    }, [roleId])
-    useEffect(() => {
-      if (skillCode){
-        
-        fetchSkill(`${viewSelectedSkillUrl}${skillCode}`)
-      }
-    }, [skillCode])
+  useEffect(() => {
+    fetchSkills(skillsUrl)
+  }, [])
+  useEffect(() => {
+    fetchCourses(coursesUrl)
+  }, [])
+  useEffect(() => {
+    setRelatedSkills(() => [])
+    if (roleId) {
 
-    useEffect(() => {
-      if (roleId){
-        console.log("retrieving data",roleId)
-        fetchRelatedSkills(`${viewSkillsByRoleUrl}${roleId}`)
-      }
-    }, [roleId])
+      fetchRole(`${viewSelectedRoleUrl}${roleId}`)
+    }
+  }, [roleId])
+  useEffect(() => {
+    if (skillCode) {
 
-    useEffect(() => {
-      if (skillCode){
-        fetchRelatedCourses(`${viewCoursesBySkillUrl}${skillCode}`)
-      }
-    }, [skillCode])
+      fetchSkill(`${viewSelectedSkillUrl}${skillCode}`)
+    }
+  }, [skillCode])
 
-    const deleteRole = async(role_id) => {
-      try {
-        const requestOptions = {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ role_id: role_id })
+  useEffect(() => {
+    if (roleId) {
+      console.log("retrieving data", roleId)
+      fetchRelatedSkills(`${viewSkillsByRoleUrl}${roleId}`)
+    }
+  }, [roleId])
+
+  useEffect(() => {
+    if (skillCode) {
+      fetchRelatedCourses(`${viewCoursesBySkillUrl}${skillCode}`)
+    }
+  }, [skillCode])
+
+  const deleteRole = async (role_id) => {
+    try {
+      const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ role_id: role_id })
       };
       fetch(deleteRoleUrl, requestOptions)
         .then(response => response.json())
     } catch (error) {
-        console.log(error.response)
+      console.log(error.response)
     }
-      const updatedRoles = roles.filter((role) => role.role_id !== role_id);
-      setRoles(updatedRoles)
-    }
+    const updatedRoles = roles.filter((role) => role.role_id !== role_id);
+    setRoles(updatedRoles)
+  }
 
-    const deleteSkill = async(skill_code) => {
-      try {
-        const requestOptions = {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ skill_code: skill_code })
+  const deleteSkill = async (skill_code) => {
+    try {
+      const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ skill_code: skill_code })
       };
       fetch(deleteSkillUrl, requestOptions)
         .then(response => response.json())
     } catch (error) {
-        console.log(error.response)
+      console.log(error.response)
     }
-      const updatedSkills = skills.filter((skill) => skill.skill_code !== skill_code);
-      setSkills(updatedSkills)
-    }
+    const updatedSkills = skills.filter((skill) => skill.skill_code !== skill_code);
+    setSkills(updatedSkills)
+  }
 
-    const updateSkill = async(formData) => {
-      try {
-        const requestOptions = {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(formData)
+  const updateSkill = async (formData) => {
+    try {
+      const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
       };
       fetch(updateSkillUrl, requestOptions)
         .then(response => response.json())
-    
-    } catch (error) {
-        console.log(error.response)
-    }
-      const updatedSkills = skills;
-      setSkills(updatedSkills)
-    }
 
-    const createSkill = async(formData) => {
-      try {
-        const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(formData)
+    } catch (error) {
+      console.log(error.response)
+    }
+    const updatedSkills = skills;
+    setSkills(updatedSkills)
+  }
+
+  const createSkill = async (formData) => {
+    try {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
       };
       fetch(createSkillUrl, requestOptions)
         .then(response => response.json())
-    
+
     } catch (error) {
-        console.log(error.response)
+      console.log(error.response)
     }
-    }
-
-
-    const selectSkill = (skill_code) => {
-      setSkillCode(skill_code)
-      setShowModal(true);
-    }
-
-    return (
-      <AppContext.Provider
-        value={{lid, ltitle, setLTitle, setLid, open, setOpen, path, setPath, roles, deleteRole, role, setRoleId, setRole, skills, deleteSkill, setSkillCode, setSkill, skill, setRoles, fetchRoles, rolesUrl, setSkills,
-          updateSkill, createSkill, activeStep, setActiveStep, skipped, setSkipped, roleId, skillCode, courses, allCourses, addCourses, setAddCourses,
-          closeModal, showModal, relatedSkills, selectSkill, ljCourses, setljCourses, fetchSkills, skillsUrl, setShowModal,
-          userDetails, setUserDetails
-}}
-      >
-        {children}
-      </AppContext.Provider>
-    )
-  }
-  
-  export const useGlobalContext = () =>{ 
-    return useContext(AppContext)
   }
 
 
-  export {AppContext, AppProvider}
+  const selectSkill = (skill_code) => {
+    setSkillCode(skill_code)
+    setShowModal(true);
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        lid, ltitle, setLTitle, setLid, open, setOpen, path, setPath, roles, deleteRole, role, setRoleId, setRole, skills, deleteSkill, setSkillCode, setSkill, skill, setRoles, fetchRoles, rolesUrl, setSkills,
+        updateSkill, createSkill, activeStep, setActiveStep, skipped, setSkipped, roleId, skillCode, courses, allCourses, addCourses, setAddCourses,
+        closeModal, showModal, relatedSkills, selectSkill, ljCourses, setljCourses, fetchSkills, skillsUrl, setShowModal,
+        userDetails, setUserDetails
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+
+export { AppContext, AppProvider }
